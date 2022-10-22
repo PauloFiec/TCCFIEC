4b0d21c3526aa20c1d5c4031d2636535b5cdbbd9
unknown <32936@fiecalunos.lan>
2022-10-21 22:18:41 -0300

    chama4

--- src/test/java/com/fiec/GSense/GSenseApplicationTests.java
+++ src/test/java/com/fiec/GSense/GSenseApplicationTests.java
@@ -3,10 +3,10 @@ package com.fiec.GSense;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class GSenseApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
