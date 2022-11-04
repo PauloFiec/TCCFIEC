7a749743a4e86ca774e47d0bcf38a820aed51b8a
gabris <66285683+ouvermax@users.noreply.github.com>
2022-11-03 21:48:56 -0300

    atualizado

--- src/main/java/com/fiec/GSense/models/dto/AuthRequestDto.java
+++ src/main/java/com/fiec/GSense/models/dto/AuthRequestDto.java
@@ -1,8 +1,10 @@
-package com.fiec.GSense.models.dto;
+package com.fiec.GSense.controllers.models.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AuthRequestDto {
     String email;
     String password;
